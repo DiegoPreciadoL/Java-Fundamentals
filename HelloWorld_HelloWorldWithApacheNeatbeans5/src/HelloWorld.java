a73243bdda3fcfe854a4545fc304bd7b4bc8cacc
Diego Preciado <diego.preciado.limon@gmail.com>
2023-01-02 19:48:24 -0600

    HelloWorld with Java

---
+++ HelloWorld_HelloWorldWithApacheNeatbeans5/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+// Java needs a class to be executed
+//  My Java Class
+public class HelloWorld {
+    
+    // All classes needs main method to can be executed
+    // Main method
+    public static void main(String args[]){
+        // Line used to print something in the console with Java languaje
+        System.out.println("Hello World From Java");
+    }
+    
+}
